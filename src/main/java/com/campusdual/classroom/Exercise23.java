d723afca46d762cf212b644ba04d38a8459329ee
HelloAdrian2k <mz.adriaan@gmail.com>
2025-01-27 11:45:23 +0100

    Ejercicio 23 finalizado.

--- src/main/java/com/campusdual/classroom/Exercise23.java
+++ src/main/java/com/campusdual/classroom/Exercise23.java
@@ -6,15 +6,33 @@ import java.util.Map;
 public class Exercise23 {
 
     public static Map<String, Person> createHashMap() {
+        Map<String, Person> map = new HashMap<>();
 
+        addMapValue(map, "person", new Person("John", "Smith"));
+        addMapValue(map, "teacher", new Teacher("María", "Montessori", "Educación"));
+        addMapValue(map, "police", new PoliceOfficer("Jake", "Peralta", "B-99"));
+        addMapValue(map, "doctor", new Doctor("Gregory","House", "Nefrología e infectología"));
+
+        return map;
     }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
-
+        map.put(key, value);
+        return value;
     }
 
     public static void printMapValues(Map<String, Person> map) {
+        for (Map.Entry<String, Person> parCV : map.entrySet()) {
+            System.out.println("Clave -> " + parCV.getKey());
+            System.out.println("Valor -> Objeto de tipo " + parCV.getValue().getClass().getSimpleName().toUpperCase());
+            System.out.print("GetDetails del objeto -> -> ");
+            parCV.getValue().getDetails();
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        printMapValues(createHashMap());
     }
     
 }
